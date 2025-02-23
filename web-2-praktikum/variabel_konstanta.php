e2974aa4c16f5da961ae9dd31812e244b6673b03
syaidahkhm8 <khumairohsyaidah@gmail.com>
2025-02-23 14:48:05 +0700

    praktikum 1

---
+++ web-2-praktikum/variabel_konstanta.php
@@ -0,0 +1,10 @@
+<?php
+define('PHI', 3.14);
+$jari_jari = 100;
+
+$luas = PHI * $jari_jari * $jari_jari;
+
+echo 'Nilai PHI adalah ' . PHI;
+echo '<br/>Luas Lingkaran dengan jari_jari ' . $jari_jari . ' adalah ' . $luas;
+
+?>
