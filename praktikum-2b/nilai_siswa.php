c8cc664043d20d7ef5053cb5c6e9186fa143cd91
syaidahkhm8 <khumairohsyaidah@gmail.com>
2025-03-04 11:50:56 +0700

    praktikum 2

---
+++ praktikum-2b/nilai_siswa.php
@@ -0,0 +1,19 @@
+<?php
+$proses = $_POST['proses'];
+$nama_siswa = $_POST['nama'];
+$mata_kuliah = $_POST['matkul'];
+$nilai_uts = $_POST['nilai_uts'];
+$nilai_uas =$_POST['nilai_uas'];
+$nilai_tugas = $_POST['nilai_tugas'];
+
+// Mencetak Hasil
+if (!empty($proses)) {
+    echo 'Proses: ' . $proses;
+    echo '<br/>Nama : ' . $nama_siswa;
+    echo '<br/>Mata Kuliah : ' . $mata_kuliah;
+    echo '<br/>Nilai UTS : ' . $nilai_uts;
+    echo '<br/>Nilai UAS : ' . $nilai_uas;
+    echo '<br/>Nilai Tugas Praktikum : ' . $nilai_tugas;
+
+
+}
