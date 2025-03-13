178b397a7d89ae22dbcfa323b61bb1c056a113b1
syaidahkhm8 <khumairohsyaidah@gmail.com>
2025-03-13 07:54:13 +0700

    praktikum03

---
+++ task/praktikum 3/partials/footer.php
@@ -0,0 +1,14 @@
+<footer class="bg-white py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto"><div class="small m-0">Dosen : Bpk. Edo Riansyah, S.Kom</div></div>
+                    <div class="col-auto">
+                        <a class="small" href="https://policies.google.com/privacy">Privacy</a>
+                        <span class="mx-1">&middot;</span>
+                        <a class="small" href="https://policies.google.com/terms">Terms</a>
+                        <span class="mx-1">&middot;</span>
+                        <a class="small" href="https://wa.me/628973780172">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
