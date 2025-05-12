f05245a9d800dc180da5b41012f094ac5b5bafc5
syaidahkhm8 <khumairohsyaidah@gmail.com>
2025-05-12 23:02:08 +0700

    praktikum06

--- Connection.php
+++ Connection.php
@@ -5,9 +5,12 @@ class Connection
 {
     public static function make($host, $db, $user, $password)
     {
+
         $dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+
         try {
             $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
             return new PDO($dsn, $user, $password, $options);
         } catch (PDOException $e) {
             die($e->getMessage());
